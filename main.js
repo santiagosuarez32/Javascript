a3c323bc32795855ff5d9a60c344f5da54a99df8
santiagosuarez32 <dlsantii86@gmail.com>
2024-09-24 13:37:50 -0300

    listo

---
+++ main.js
@@ -0,0 +1,9 @@
+/* console.log (`Algo "Algo"`) */
+
+// lET O CONST
+
+/* let nombre = `Messi`
+console.log(`Hola soy ${nombre}`)
+console.log(`Hola soy ` + nombre) */
+
+let nombreUsuario = prompt(`Me queres?`)
